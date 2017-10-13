3486942d820fb14898193975af63963590f06322
Rehan Dalal <rehandalal@gmail.com>
2017-10-13 03:09:01 -0400

    Move addon details out of package.json

--- bin/make_xpi.sh
+++ bin/make_xpi.sh
@@ -17,8 +17,8 @@ function cleanup {
 trap cleanup EXIT
 
 # fill templates, could be fancier
-node_modules/.bin/mustache package.json templates/install.rdf.mustache > "${DEST}/install.rdf"
-node_modules/.bin/mustache package.json templates/chrome.manifest.mustache > "${DEST}/chrome.manifest"
+node_modules/.bin/mustache addon.json templates/install.rdf.mustache > "${DEST}/install.rdf"
+node_modules/.bin/mustache addon.json templates/chrome.manifest.mustache > "${DEST}/chrome.manifest"
 cp vendor/PioneerUtils.jsm "${DEST}"
 
 cp -rp src/* "$DEST"
