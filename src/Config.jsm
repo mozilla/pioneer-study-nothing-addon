3708975db2c1a1db4febafc7665796410663274d
Rehan Dalal <rehandalal@gmail.com>
2017-12-04 18:48:20 -0500

    Refactor addon to bring up to new specs

--- src/Config.jsm
+++ src/Config.jsm
@@ -1,18 +1,23 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "(config|EXPORTED_SYMBOLS)" }]*/
 
 const { utils: Cu } = Components;
+Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "AddonManager", "resource://gre/modules/AddonManager.jsm");
+
+XPCOMUtils.defineLazyModuleGetter(
+  this, "AddonManager", "resource://gre/modules/AddonManager.jsm"
+);
 
 const EXPORTED_SYMBOLS = ["config"];
 
+const TELEMETRY_ENV_PREF = "extensions.pioneer-study-nothing.telemetryEnv";
+
+
 const config = {
-  pioneer: {
-    studyName: "example",  // this name must match the pipeline schema to be used
-    studyVersion: 1,
-  },
-  async isEligible() {
-    const addon = await AddonManager.getAddonByID("pioneer-opt-in@mozilla.org");
-    return addon !== null;
-  },
+  addonId: "nothing-study@pioneer.mozilla.org",
+  studyName: "nothing",
+  branches: [
+    { name: "default", weight: 1 },
+  ],
+  telemetryEnv: Services.prefs.getCharPref(TELEMETRY_ENV_PREF, "prod"),
 };
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,9 +3,15 @@ Cu.import("resource://gre/modules/AddonManager.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-XPCOMUtils.defineLazyModuleGetter(this, "config", "resource://pioneer-study-nothing/Config.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "PioneerUtils",
-  "resource://pioneer-study-nothing/PioneerUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(
+  this, "config", "resource://pioneer-study-nothing/Config.jsm"
+);
+XPCOMUtils.defineLazyModuleGetter(
+  this, "PioneerUtils", "resource://pioneer-study-nothing/PioneerUtils.jsm"
+);
+XPCOMUtils.defineLazyModuleGetter(
+  this, "PrefUtils", "resource://pioneer-study-nothing/lib/PrefUtils.jsm"
+);
 
 const REASONS = {
   APP_STARTUP:      1, // The application is starting up.
@@ -21,45 +27,40 @@ const PING_SENT_PREF = "extensions.pioneer-study-nothing.pingSent";
 const EXPIRATION_DATE_PREF = "extensions.pioneer-study-nothing.expirationDateString";
 
 
-function uninstallAddon(id) {
-  AddonManager.getAddonByID(id, addon => addon.uninstall());
-}
-
-
 this.install = function() {};
 
 
 this.startup = async function(data, reason) {
-  const pioneerUtils = new PioneerUtils(config.pioneer);
+  const pioneerUtils = new PioneerUtils(config);
+  const events = pioneerUtils.getAvailableEvents();
+
+  const isEligible = await pioneerUtils.isUserOptedIn();
+  if (!isEligible) {
+    pioneerUtils.endStudy(events.INELIGIBLE);
+    return;
+  }
 
   // Always set EXPIRATION_DATE_PREF if it not set, even if outside of install.
   // This is a failsafe if opt-out expiration doesn't work, so should be resilient.
-  // Also helps for testing.
-  if (!Services.prefs.prefHasUserValue(EXPIRATION_DATE_PREF)) {
-    const now = new Date(Date.now());
-    const expirationDateString = new Date(now.setDate(now.getDate() + 7)).toISOString();
-    Services.prefs.setCharPref(EXPIRATION_DATE_PREF, expirationDateString);
+  let expirationDate = PrefUtils.getLongPref(EXPIRATION_DATE_PREF, 0);
+  if (!expirationDate) {
+    const phases = Object.values(Config.phases);
+    const studyLength = phases.map(p => p.duration || 0).reduce((a, b) => a + b);
+    expirationDate = Date.now() + studyLength;
+    PrefUtils.setLongPref(EXPIRATION_DATE_PREF, expirationDate);
   }
 
-  if (reason === REASONS.ADDON_INSTALL) {
-    const eligible = await config.isEligible(); // addon-specific
-    if (!eligible) {
-      uninstallAddon(data.id);
-      return;
-    }
-  }
-
-  const expirationDate = new Date(Services.prefs.getCharPref(EXPIRATION_DATE_PREF));
+  // Check if the study has expired
   if (Date.now() > expirationDate) {
-    uninstallAddon(data.id);
+    pioneerUtils.endStudy(events.EXPIRED);
+    return;
   }
 
   if (reason === REASONS.ADDON_INSTALL && !Services.prefs.getBoolPref(PING_SENT_PREF, false)) {
     const payload = {
-      exampleString: `${Date.now()}`,
+      eventId: "enrolled",
     };
-
-    pioneerUtils.submitEncryptedPing(payload).then(function() {
+    pioneerUtils.submitEncryptedPing("event", 1, payload).then(function() {
       Services.prefs.setBoolPref(PING_SENT_PREF, true);
     });
   }
@@ -69,6 +70,7 @@ this.startup = async function(data, reason) {
 this.shutdown = async function(data, reason) {
   Cu.unload("resource://pioneer-study-nothing/Config.jsm");
   Cu.unload("resource://pioneer-study-nothing/PioneerUtils.jsm");
+  Cu.unload("resource://pioneer-study-nothing/lib/PrefUtils.jsm");
 };
 
 
